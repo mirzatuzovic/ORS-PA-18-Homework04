27fa23fd0e703bf7c6711c2e38ef4b47d0453c9d
Mirza Tuzovic <Mirza.Tuzovic@udg.edu.me>
2019-03-13 17:14:19 +0100

    task 2

--- task2.py
+++ task2.py
@@ -15,3 +15,24 @@
 * in which your solution might not work.
 ===================================================
 """
+
+broj_elemenata_niza = int(input("Unesite broj elemenata niza:"))
+
+niz =[]
+parni = 0
+neparni = 0
+for i in range(broj_elemenata_niza):
+
+   element_niza = int(input("Unesite novi element:"))
+   niz.append(element_niza)
+
+
+for element_niza in niz:
+    if(element_niza % 2) == 0:
+        parni +=1
+    else:
+        neparni += 1
+
+print(niz)
+print("Parnih je:", parni)
+print("Neparnih je:" , neparni)
